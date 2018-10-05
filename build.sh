48d4c39076fa56a0e4a3324bf2849b50c6a0dca2
Sandra Dylus <sad@informatik.uni-kiel.de>
2018-10-05 14:13:02 +0200

    Update GHC version

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p _shake
 mkdir -p dist
-ghc -package-db=/Users/sad/Documents/Programming/Haskell/Shake/.cabal-sandbox/x86_64-osx-ghc-8.0.2-packages.conf.d/ --make Build.hs -rtsopts -threaded -with-rtsopts=-I0 -outputdir=_shake -o _shake/build && _shake/build "$@"
+ghc -package-db=/Users/sad/Documents/Programming/Haskell/Shake/.cabal-sandbox/x86_64-osx-ghc-8.4.3-packages.conf.d/ --make Build.hs -rtsopts -threaded -with-rtsopts=-I0 -outputdir=_shake -o _shake/build && _shake/build "$@"
