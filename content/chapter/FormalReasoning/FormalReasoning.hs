47872186cdc8726b469beefc84ac04600d73e5a3
Sandra Dylus <sad@informatik.uni-kiel.de>
2019-04-09 17:22:14 +0200

    Move exemplary used code in seperate files for type-checking

--- content/chapter/FormalReasoning/FormalReasoning.hs
+++ content/chapter/FormalReasoning/FormalReasoning.hs
@@ -1,5 +1,5 @@
 infixl 4 +++
 
 (+++) :: [a] -> [a] -> [a]
-[]     +++ ys = ys
-x : xs +++ ys = x : (xs +++ ys)
+(+++) []       ys = ys
+(+++) (x : xs) ys = x : ((+++) xs ys)
--- content/chapter/FormalReasoning/FormalReasoning.v
+++ content/chapter/FormalReasoning/FormalReasoning.v
@@ -1,81 +1,184 @@
 Set Implicit Arguments.
-Require Import List.
-Print hd_error.
-Search (forall a, _ -> list a -> a).
-Print hd.
-Check head.
+(* Require Import List. *)
 
-Fail Definition exampleList (A : Type) : list A := cons (head (nil : list A)) nil.
+(* Fail Definition exampleList (A : Type) : list A := cons (head (nil : list A)) nil. *)
+
+Module Option.
+
+  Inductive List (A : Type) :=
+  | nil : List A
+  | cons : option A -> option (List A) -> List A.
+  
+  Arguments nil {_}.
+  
+  Definition Nil (A : Type) : option (List A) :=
+    Some nil.
+  Definition Cons (A : Type) (ox : option A) (oxs : option (List A))
+    : option (List A) :=
+    Some (cons ox oxs).
+  
+  Definition head (A : Type) (oxs : option (List A)) : option A :=
+    match oxs with
+    | None    => None
+    | Some xs => match xs with
+                | nil       => None
+                | cons ox _ => ox
+                end
+    end.
+  
+  Fail Fixpoint append (A : Type) (oxs oys : option (List A)) {struct oxs} : option (List A) :=
+    match oxs with
+    | None    => None
+    | Some xs => match xs with
+                | nil         => oys
+                | cons oz ozs => Cons oz (append ozs oys)
+                end
+    end.
+  
+  Fail Fixpoint append (A : Type) (oxs oys : option (List A)) {struct oxs} : option (List A) :=
+    match oxs with
+    | None    => None
+    | Some xs =>
+      let fix append' xs oys :=
+          match xs with
+          | nil         => oys
+          | cons oz ozs => Cons oz (append ozs oys)
+          end
+      in append' xs oys
+    end.
+  
+  Definition append_ (A : Type) (oxs oys : option (List A)) : option (List A) :=
+    match oxs with
+    | None    => None
+    | Some xs =>
+      let fix append' xs oys :=
+          match xs with
+          | nil         => oys
+          | cons oz ozs => Cons oz (match ozs with
+                                    | None => None
+                                    | Some zs => append' zs oys
+                                    end)
+          end
+      in append' xs oys
+    end.
+  
+  
+  
+  
+  Fixpoint append' (A : Type) (xs : List A) (oys : option (List A)) : option (List A) :=
+    match xs with
+    | nil         => oys
+    | cons oz ozs => Cons oz (match ozs with
+                              | None    => None
+                              | Some zs => append' zs oys
+                              end)
+    end.
+  
+  Definition append (A : Type) (oxs oys : option (List A)) : option (List A) :=
+    match oxs with
+    | None    => None
+    | Some xs => append' xs oys
+    end.
+
+End Option.
+
+Module Partial.
+
+Inductive partial (A : Type) :=
+| undefined : partial A
+| defined   :  A -> partial A.
+
+Arguments undefined {_}.
 
 Inductive List (A : Type) :=
-| nil_ : List A
-| cons_ : option A -> option (List A) -> List A.
+| nil  : List A
+| cons : partial A -> partial (List A) -> List A.
 
-Arguments nil_ {_}.
+Arguments nil {_}.
 
-Definition Nil (A : Type) : option (List A) :=
-  Some nil_.
-Definition Cons (A : Type) (ox : option A) (oxs : option (List A))
-  : option (List A) :=
-  Some (cons_ ox oxs).
+Definition Nil (A : Type) : partial (List A) :=
+  defined nil.
+Definition Cons (A : Type) (ox : partial A) (oxs : partial (List A))
+  : partial (List A) :=
+  defined (cons ox oxs).
 
-Definition head (A : Type) (oxs : option (List A)) : option A :=
+Definition head (A : Type) (oxs : partial (List A)) : partial A :=
   match oxs with
-  | None    => None
-  | Some xs => match xs with
-              | nil_       => None
-              | cons_ ox _ => ox
-              end
+  | undefined  => undefined
+  | defined xs => match xs with
+                 | nil       => undefined
+                 | cons ox _ => ox
+                 end
   end.
 
-Fail Fixpoint append (A : Type) (oxs oys : option (List A)) {struct oxs} : option (List A) :=
+Fail Fixpoint append (A : Type) (oxs oys : partial (List A)) {struct oxs} : partial (List A) :=
   match oxs with
-  | None    => None
-  | Some xs => match xs with
-              | nil_         => oys
-              | cons_ oz ozs => Cons oz (append ozs oys)
-              end
+  | undefined  => undefined
+  | defined xs => match xs with
+                 | nil         => oys
+                 | cons oz ozs => Cons oz (append ozs oys)
+                 end
   end.
 
-Fail Fixpoint append (A : Type) (oxs oys : option (List A)) {struct oxs} : option (List A) :=
+Fail Fixpoint append (A : Type) (oxs oys : partial (List A)) {struct oxs} : partial (List A) :=
   match oxs with
-  | None    => None
-  | Some xs =>
+  | undefined  => undefined
+  | defined xs =>
     let fix append' xs oys :=
         match xs with
-        | nil_         => oys
-        | cons_ oz ozs => Cons oz (append ozs oys)
+        | nil         => oys
+        | cons oz ozs => Cons oz (append ozs oys)
         end
     in append' xs oys
   end.
 
-Definition append_ (A : Type) (oxs oys : option (List A)) : option (List A) :=
+Definition append_ (A : Type) (oxs oys : partial (List A)) : partial (List A) :=
   match oxs with
-  | None    => None
-  | Some xs =>
+  | undefined  => undefined
+  | defined xs =>
     let fix append' xs oys :=
         match xs with
-        | nil_         => oys
-        | cons_ oz ozs => Cons oz (match ozs with
-                                  | None => None
-                                  | Some zs => append' zs oys
+        | nil         => oys
+        | cons oz ozs => Cons oz (match ozs with
+                                  | undefined => undefined
+                                  | defined zs => append' zs oys
                                   end)
         end
     in append' xs oys
   end.
 
-
-Fixpoint append' (A : Type) (xs : List A) (oys : option (List A)) : option (List A) :=
+Fixpoint append' (A : Type) (xs : List A) (oys : partial (List A)) : partial (List A) :=
   match xs with
-  | nil_         => oys
-  | cons_ oz ozs => Cons oz (match ozs with
-                            | None    => None
-                            | Some zs => append' zs oys
+  | nil         => oys
+  | cons oz ozs => Cons oz (match ozs with
+                            | undefined    => undefined
+                            | defined zs => append' zs oys
                             end)
   end.
 
-Definition append (A : Type) (oxs oys : option (List A)) : option (List A) :=
+Definition append (A : Type) (oxs oys : partial (List A)) : partial (List A) :=
   match oxs with
-  | None    => None
-  | Some xs => append' xs oys
+  | undefined  => undefined
+  | defined xs => append' xs oys
+  end.
+
+End Partial.
+
+Module General.
+
+Fail Inductive List (M : Type -> Type) (A : Type) :=
+| nil : List M A
+| cons : M A -> M (List M A) -> List M A.
+
+End General.
+
+Fail Inductive NonStrictlyPos :=
+| con : (NonStrictlyPos -> nat) -> NonStrictlyPos.
+
+Inductive StrictlyPos :=
+| con : StrictlyPos -> (nat -> StrictlyPos) -> StrictlyPos.
+
+Fail Definition applyFun (t : NonStrictlyPos) : nat :=
+  match t with
+  | con f => f t
   end.
