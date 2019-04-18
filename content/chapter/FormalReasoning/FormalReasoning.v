e10d013e1736ae590ebc6218bbd44bb2c710b6cd
Sandra Dylus <sad@informatik.uni-kiel.de>
2019-04-18 18:23:11 +0200

    Add alternative free_bind definition and fix indentation
    #68

--- content/chapter/FormalReasoning/FormalReasoning.v
+++ content/chapter/FormalReasoning/FormalReasoning.v
@@ -87,130 +87,139 @@ End Option.
 
 Module Partial.
 
-Inductive partial (A : Type) :=
-| undefined : partial A
-| defined   :  A -> partial A.
+  Inductive partial (A : Type) :=
+  | undefined : partial A
+  | defined   :  A -> partial A.
 
-Arguments undefined {_}.
+  Arguments undefined {_}.
 
-Inductive List (A : Type) :=
-| nil  : List A
-| cons : partial A -> partial (List A) -> List A.
+  Inductive List (A : Type) :=
+  | nil  : List A
+  | cons : partial A -> partial (List A) -> List A.
 
-Arguments nil {_}.
+  Arguments nil {_}.
 
-Definition Nil (A : Type) : partial (List A) :=
-  defined nil.
-Definition Cons (A : Type) (ox : partial A) (oxs : partial (List A))
-  : partial (List A) :=
-  defined (cons ox oxs).
+  Definition Nil (A : Type) : partial (List A) :=
+    defined nil.
+  Definition Cons (A : Type) (ox : partial A) (oxs : partial (List A))
+    : partial (List A) :=
+    defined (cons ox oxs).
 
-Definition head (A : Type) (oxs : partial (List A)) : partial A :=
-  match oxs with
-  | undefined  => undefined
-  | defined xs => match xs with
-                 | nil       => undefined
-                 | cons ox _ => ox
-                 end
-  end.
+  Definition head (A : Type) (oxs : partial (List A)) : partial A :=
+    match oxs with
+    | undefined  => undefined
+    | defined xs => match xs with
+                   | nil       => undefined
+                   | cons ox _ => ox
+                   end
+    end.
 
-Fail Fixpoint append (A : Type) (oxs oys : partial (List A)) {struct oxs} : partial (List A) :=
-  match oxs with
-  | undefined  => undefined
-  | defined xs => match xs with
-                 | nil         => oys
-                 | cons oz ozs => Cons oz (append ozs oys)
-                 end
-  end.
+  Fail Fixpoint append (A : Type) (oxs oys : partial (List A)) {struct oxs} : partial (List A) :=
+    match oxs with
+    | undefined  => undefined
+    | defined xs => match xs with
+                   | nil         => oys
+                   | cons oz ozs => Cons oz (append ozs oys)
+                   end
+    end.
 
-Fail Fixpoint append (A : Type) (oxs oys : partial (List A)) {struct oxs} : partial (List A) :=
-  match oxs with
-  | undefined  => undefined
-  | defined xs =>
-    let fix append' xs oys :=
-        match xs with
-        | nil         => oys
-        | cons oz ozs => Cons oz (append ozs oys)
-        end
-    in append' xs oys
-  end.
+  Fail Fixpoint append (A : Type) (oxs oys : partial (List A)) {struct oxs} : partial (List A) :=
+    match oxs with
+    | undefined  => undefined
+    | defined xs =>
+      let fix append' xs oys :=
+          match xs with
+          | nil         => oys
+          | cons oz ozs => Cons oz (append ozs oys)
+          end
+      in append' xs oys
+    end.
 
-Definition append_ (A : Type) (oxs oys : partial (List A)) : partial (List A) :=
-  match oxs with
-  | undefined  => undefined
-  | defined xs =>
-    let fix append' xs oys :=
-        match xs with
-        | nil         => oys
-        | cons oz ozs => Cons oz (match ozs with
-                                  | undefined => undefined
-                                  | defined zs => append' zs oys
-                                  end)
-        end
-    in append' xs oys
-  end.
+  Definition append_ (A : Type) (oxs oys : partial (List A)) : partial (List A) :=
+    match oxs with
+    | undefined  => undefined
+    | defined xs =>
+      let fix append' xs oys :=
+          match xs with
+          | nil         => oys
+          | cons oz ozs => Cons oz (match ozs with
+                                   | undefined => undefined
+                                   | defined zs => append' zs oys
+                                   end)
+          end
+      in append' xs oys
+    end.
 
-Fixpoint append' (A : Type) (xs : List A) (oys : partial (List A)) : partial (List A) :=
-  match xs with
-  | nil         => oys
-  | cons oz ozs => Cons oz (match ozs with
-                            | undefined    => undefined
-                            | defined zs => append' zs oys
-                            end)
-  end.
+  Fixpoint append' (A : Type) (xs : List A) (oys : partial (List A)) : partial (List A) :=
+    match xs with
+    | nil         => oys
+    | cons oz ozs => Cons oz (match ozs with
+                             | undefined    => undefined
+                             | defined zs => append' zs oys
+                             end)
+    end.
 
-Definition append (A : Type) (oxs oys : partial (List A)) : partial (List A) :=
-  match oxs with
-  | undefined  => undefined
-  | defined xs => append' xs oys
-  end.
+  Definition append (A : Type) (oxs oys : partial (List A)) : partial (List A) :=
+    match oxs with
+    | undefined  => undefined
+    | defined xs => append' xs oys
+    end.
 
 End Partial.
 
 Module Total.
 
-Inductive total (A : Type) :=
-| totality : A -> total A.
-
+  Inductive total (A : Type) :=
+  | totality : A -> total A.
+  
 End Total.
 
 Module GenericList.
 
-Fail Inductive List (M : Type -> Type) (A : Type) :=
-| nil : List M A
-| cons : M A -> M (List M A) -> List M A.
+  Fail Inductive List (M : Type -> Type) (A : Type) :=
+  | nil : List M A
+  | cons : M A -> M (List M A) -> List M A.
 
 End GenericList.
 
-Fail Inductive NonStrictlyPos :=
-| con : (NonStrictlyPos -> nat) -> NonStrictlyPos.
+Module StrictPos.
 
-Inductive StrictlyPos :=
-| con : StrictlyPos -> (nat -> StrictlyPos) -> StrictlyPos.
+  Fail Inductive NonStrictlyPos :=
+  | con : (NonStrictlyPos -> nat) -> NonStrictlyPos.
 
-Fail Definition applyFun (t : NonStrictlyPos) : nat :=
-  match t with
-  | con f => f t
-  end.
+  Inductive StrictlyPos :=
+  | con : StrictlyPos -> (nat -> StrictlyPos) -> StrictlyPos.
 
-Fail Inductive Mu A :=
-| mu : (Mu A -> A) -> Mu A.
+  Fail Definition applyFun (t : NonStrictlyPos) : nat :=
+    match t with
+    | con f => f t
+    end.
 
-Definition Cont R A := (A -> R) -> R.
+  Fail Inductive Mu A :=
+  | mu : (Mu A -> A) -> Mu A.
 
-Fail Inductive ListCont R A :=
-| nilC  : ListCont R A
-| consC : ((A -> R) -> R) -> ((ListCont R A -> R) -> R) -> ListCont R A.
+  Definition Cont R A := (A -> R) -> R.
 
+  Fail Inductive ListCont R A :=
+  | nilC  : ListCont R A
+  | consC : ((A -> R) -> R) -> ((ListCont R A -> R) -> R) -> ListCont R A.
+
+End StrictPos.
+  
 Module SigmaUniverse.
 
-  Variable U : Type.
-  Variable El : U -> Type.
+  Section ExtSigma.
 
-  Inductive ExtSigma :=
-  | extsigma : forall (a : U), ((El a -> U) -> U) -> ExtSigma.
+    Variable U : Type.
+    Variable El : U -> Type.
+
+    Inductive ExtSigma :=
+    | extsigma : forall (a : U), ((El a -> U) -> U) -> ExtSigma.
+
+  End ExtSigma.
 
 End SigmaUniverse.
+
 Module General.
 
   Inductive General (S : Type) (T : S -> Type) A :=
@@ -250,60 +259,84 @@ Module General.
       | suc n => call n >>= fun fn => call fn >>= fun ffn => res (suc ffn)
       end.
 
-  Compute (fusc zero).
-  Compute (fusc (suc zero)).
-  Compute (fusc (suc (suc zero))).
+  (* Compute (fusc zero). *)
+  (* Compute (fusc (suc zero)). *)
+  (* Compute (fusc (suc (suc zero))). *)
   
 End General.
   
 Module Container.
 
-Inductive Ext (Shape : Type) (Pos : Shape -> Type) A :=
-| ext : forall s, (Pos s -> A) -> Ext Pos A.
+  Inductive Ext (Shape : Type) (Pos : Shape -> Type) A :=
+  | ext : forall s, (Pos s -> A) -> Ext Pos A.
 
-Arguments Ext Shape Pos A : clear implicits.
-Arguments ext {_} {_} {_}.
+  Arguments Ext Shape Pos A : clear implicits.
+  Arguments ext {_} {_} {_}.
 
 End Container.
 
-
 Module Free.
 
-Fail Inductive Free F A :=
-| pure   : A -> Free F A
-| impure : F (Free F A) -> Free F A.
+  Fail Inductive Free F A :=
+  | pure   : A -> Free F A
+  | impure : F (Free F A) -> Free F A.
 
-Import Container.
+  Import Container.
 
-Inductive Free (Shape : Type) (Pos : Shape -> Type) A :=
-| pure   : A -> Free Pos A
-| impure : Ext Shape Pos (Free Pos A) -> Free Pos A.
+  Inductive Free (Shape : Type) (Pos : Shape -> Type) A :=
+  | pure   : A -> Free Pos A
+  | impure : Ext Shape Pos (Free Pos A) -> Free Pos A.
 
-Arguments Free : clear implicits.
+  Arguments Free : clear implicits.
 
-Arguments pure {_} {_} {_}.
+  Arguments pure {_} {_} {_}.
 
-Section bind.
+  Module withSection.
 
-  Variable Shape : Type.
-  Variable Pos : Shape -> Type.
+    Section Args.
+      
+      Variable Shape : Type.
+      Variable Pos : Shape -> Type.
 
-  Section local.
+      Section free_bind.
 
-    Variable A B : Type.
-    Variable f : A -> Free Shape Pos B.
+        Variable A B : Type.
+        Variable f : A -> Free Shape Pos B.
 
-    Fixpoint free_bind (fx : Free Shape Pos A) : Free Shape Pos B :=
-      match fx with
-      | pure x => f x
-      | impure (ext s pf) => impure (ext s (fun p => free_bind (pf p)))
-      end.
+        Fixpoint free_bind (fx : Free Shape Pos A) : Free Shape Pos B :=
+          match fx with
+          | pure x => f x
+          | impure (ext s pf) => impure (ext s (fun p => free_bind (pf p)))
+          end.
 
-  End local.
+      End free_bind.
 
-End bind.
+    End Args.
 
-Notation "fx >>= f" := (free_bind f fx) (at level 40, left associativity).
+  End withSection.
+  Module withLocal.
+
+    Section Args.
+      
+      Variable Shape : Type.
+      Variable Pos : Shape -> Type.
+
+      Definition free_bind A B (f : A -> Free Shape Pos B) (fx : Free Shape Pos A)
+      : Free Shape Pos B :=
+        let fix free_bind' fx :=
+            match fx with
+            | pure x            => f x
+            | impure (ext s pf) => impure (ext s (fun p => free_bind' (pf p)))
+            end
+        in free_bind' fx.
+
+    End Args.
+
+  End withLocal.
+
+  Import withLocal.
+  
+  Notation "fx >>= f" := (free_bind f fx) (at level 40, left associativity).
 
 End Free.
 
@@ -311,183 +344,188 @@ Inductive Empty : Type := .
 
 Module Partiality.
 
-Import Container.
+  Import Container.
 
-Inductive One (A : Type) :=
-| one : One A.
+  Inductive One (A : Type) :=
+  | one : One A.
 
-Arguments one {_}.
+  Arguments one {_}.
 
-Definition One__S := unit.
-Definition One__P (s : One__S) := Empty.
+  Definition One__S := unit.
+  Definition One__P (s : One__S) := Empty.
 
-Definition from_One A (o : One A) : Ext One__S One__P A :=
-  ext tt (fun (p : One__P tt) => match p with end).
+  Definition from_One A (o : One A) : Ext One__S One__P A :=
+    ext tt (fun (p : One__P tt) => match p with end).
 
-Definition to_One A (e : Ext One__S One__P A) : One A :=
-  one.
+  Definition to_One A (e : Ext One__S One__P A) : One A :=
+    one.
 
-Arguments from_One / _ _ .
-Arguments to_One / _ _.
+  Arguments from_One / _ _ .
+  Arguments to_One / _ _.
 
-Lemma from_to_One : forall (A : Type) (e : Ext One__S One__P A),
-    from_One (to_One e) = e.
-Proof.
-  intros A e.
-  destruct e as [[] pf]; simpl.
-  f_equal. extensionality p. destruct p.
-Qed.
+  Lemma from_to_One : forall (A : Type) (e : Ext One__S One__P A),
+      from_One (to_One e) = e.
+  Proof.
+    intros A e.
+    destruct e as [[] pf]; simpl.
+    f_equal. extensionality p. destruct p.
+  Qed.
 
-Lemma to_from_One : forall (A : Type) (o : One A),
-    to_One (from_One o) = o.
-Proof.
-  intros A o.
-  destruct o; reflexivity.
-Qed.
+  Lemma to_from_One : forall (A : Type) (o : One A),
+      to_One (from_One o) = o.
+  Proof.
+    intros A o.
+    destruct o; reflexivity.
+  Qed.
 
-Import Partial.
-Import Free.
+  Import Partial.
+  Import Free.
 
-Definition to_partial A (fx : Free One__S One__P A) : partial A :=
-  match fx with
-  | pure x   => defined x
-  | impure e => undefined
-  end.
+  Definition to_partial A (fx : Free One__S One__P A) : partial A :=
+    match fx with
+    | pure x   => defined x
+    | impure e => undefined
+    end.
 
-Definition from_partial A (p : partial A) : Free One__S One__P A :=
-  match p with
-  | undefined => impure (from_One one)
-  | defined x => pure x
-  end.
+  Definition from_partial A (p : partial A) : Free One__S One__P A :=
+    match p with
+    | undefined => impure (from_One one)
+    | defined x => pure x
+    end.
 
-Lemma from_to_partial : forall (A : Type) (fx : Free One__S One__P A),
-    from_partial (to_partial fx) = fx.
-Proof.
-  intros A fx.
-  destruct fx as [x | [[] pf]]; simpl.
-  - reflexivity.
-  - do 2 f_equal. extensionality p. destruct p.
-Qed.
+  Lemma from_to_partial : forall (A : Type) (fx : Free One__S One__P A),
+      from_partial (to_partial fx) = fx.
+  Proof.
+    intros A fx.
+    destruct fx as [x | [[] pf]]; simpl.
+    - reflexivity.
+    - do 2 f_equal. extensionality p. destruct p.
+  Qed.
 
-Lemma to_from_partial : forall (A : Type) (p : partial A),
-    to_partial (from_partial p) = p.
-Proof.
-  intros A p.
-  destruct p; reflexivity.
-Qed.
+  Lemma to_from_partial : forall (A : Type) (p : partial A),
+      to_partial (from_partial p) = p.
+  Proof.
+    intros A p.
+    destruct p; reflexivity.
+  Qed.
 
 End Partiality.
 
 Module Totality.
 
-Import Container.
+  Import Container.
 
-Inductive Zero (A : Type) := .
+  Inductive Zero (A : Type) := .
 
-Definition Zero__S := Empty.
-Definition Zero__P (s : Zero__S) := Empty.
+  Definition Zero__S := Empty.
+  Definition Zero__P (s : Zero__S) := Empty.
 
-Definition from_Zero A (u : Zero A) : Ext Zero__S Zero__P A :=
-  match u with end.
+  Definition from_Zero A (u : Zero A) : Ext Zero__S Zero__P A :=
+    match u with end.
 
-Definition to_Zero A (e : Ext Zero__S Zero__P A) : Zero A :=
-  match e with
-  | ext s pf => match s with end
-  end.
+  Definition to_Zero A (e : Ext Zero__S Zero__P A) : Zero A :=
+    match e with
+    | ext s pf => match s with end
+    end.
 
-Arguments from_Zero / _ _ .
-Arguments to_Zero / _ _.
+  Arguments from_Zero / _ _ .
+  Arguments to_Zero / _ _.
 
-Lemma from_to_Zero : forall (A : Type) (e : Ext Zero__S Zero__P A),
-    from_Zero (to_Zero e) = e.
-Proof.
-  intros A e.
-  destruct e as [[] pf].
-Qed.
+  Lemma from_to_Zero : forall (A : Type) (e : Ext Zero__S Zero__P A),
+      from_Zero (to_Zero e) = e.
+  Proof.
+    intros A e.
+    destruct e as [[] pf].
+  Qed.
 
-Lemma to_from_Zero : forall (A : Type) (z : Zero A),
-    to_Zero (from_Zero z) = z.
-Proof.
-  intros A z.
-  destruct z.
-Qed.
+  Lemma to_from_Zero : forall (A : Type) (z : Zero A),
+      to_Zero (from_Zero z) = z.
+  Proof.
+    intros A z.
+    destruct z.
+  Qed.
 
-Import Total.
-Import Free.
+  Import Total.
+  Import Free.
 
-Definition to_total A (fx : Free Zero__S Zero__P A) : total A :=
-  match fx with
-  | pure x   => totality x
-  | impure (ext s _) => match s with end
-  end.
+  Definition to_total A (fx : Free Zero__S Zero__P A) : total A :=
+    match fx with
+    | pure x   => totality x
+    | impure (ext s _) => match s with end
+    end.
 
-Definition from_total A (t : total A) : Free Zero__S Zero__P A :=
-  match t with
-  | totality x => pure x
-  end.
+  Definition from_total A (t : total A) : Free Zero__S Zero__P A :=
+    match t with
+    | totality x => pure x
+    end.
 
-Lemma from_to_total : forall (A : Type) (fx : Free Zero__S Zero__P A),
-    from_total (to_total fx) = fx.
-Proof.
-  intros A fx.
-  destruct fx as [x | [[] pf]]; reflexivity.
-Qed.
+  Lemma from_to_total : forall (A : Type) (fx : Free Zero__S Zero__P A),
+      from_total (to_total fx) = fx.
+  Proof.
+    intros A fx.
+    destruct fx as [x | [[] pf]]; reflexivity.
+  Qed.
 
-Lemma to_from_total : forall (A : Type) (t : total A),
-    to_total (from_total t) = t.
-Proof.
-  intros A t.
-  destruct t; reflexivity.
-Qed.
+  Lemma to_from_total : forall (A : Type) (t : total A),
+      to_total (from_total t) = t.
+  Proof.
+    intros A t.
+    destruct t; reflexivity.
+  Qed.
 
 End Totality.
 
 Section FreeList.
 
-Import Free.
-Import Container.
+  Import Free.
+  Import Container.
 
-Inductive List (Shape : Type) (Pos : Shape -> Type) A :=
-| nil : List Pos A
-| cons : Free Shape Pos A -> Free Shape Pos (List Pos A) -> List Pos A.
+  Inductive List (Shape : Type) (Pos : Shape -> Type) A :=
+  | nil : List Pos A
+  | cons : Free Shape Pos A -> Free Shape Pos (List Pos A) -> List Pos A.
 
-Arguments List Shape Pos A : clear implicits.
-Arguments nil {_} {_} {_}.
+  Arguments List Shape Pos A : clear implicits.
+  Arguments nil {_} {_} {_}.
 
-Section append.
+  Section append.
 
-Variable S : Type.
-Variable P : S -> Type.
+    Variable S : Type.
+    Variable P : S -> Type.
 
-Fail Fixpoint append' A (xs : List S P A) (fys : Free S P (List S P A)) :=
-  match xs with
-  | nil => fys
-  | cons fz fzs => pure (cons fz (match fzs with
-                                 | pure zs           => append' zs fys
-                                 | impure (ext s pf) => _ (* what to do here? *)
-                                 end))
-  end.
+    Fail Fixpoint append' A (xs : List S P A) (fys : Free S P (List S P A)) :=
+      match xs with
+      | nil => fys
+      | cons fz fzs => pure (cons fz (match fzs with
+                                     | pure zs           => append' zs fys
+                                     | impure (ext s pf) => _ (* what to do here? *)
+                                     end))
+      end.
 
-Fixpoint free_bind A B (fx : Free S P A) (f : A -> Free S P B) : Free S P B :=
-  match fx with
-  | pure x            => f x
-  | impure (ext s pf) => impure (ext s (fun p => free_bind (pf p) f))
-  end.
+    Fixpoint free_bind A B (fx : Free S P A) (f : A -> Free S P B) : Free S P B :=
+      match fx with
+      | pure x            => f x
+      | impure (ext s pf) => impure (ext s (fun p => free_bind (pf p) f))
+      end.
 
-Fail Fixpoint append' A (xs : List S P A) (fys : Free S P (List S P A)) {struct xs}:=
-  match xs with
-  | nil => fys
-  | cons fz fzs => pure (cons fz (free_bind fzs (fun zs => append' zs fys)))
-  end.
+    Fail Fixpoint append' A (xs : List S P A) (fys : Free S P (List S P A)) {struct xs}:=
+      match xs with
+      | nil => fys
+      | cons fz fzs => pure (cons fz (free_bind fzs (fun zs => append' zs fys)))
+      end.
 
-Fixpoint append' A (xs : List S P A) (fys : Free S P (List S P A)) :=
-  match xs with
-  | nil => fys
-  | cons fz fzs => pure (cons fz (fzs >>= fun zs => append' zs fys))
-  end.
+    Fixpoint append' A (xs : List S P A) (fys : Free S P (List S P A)) :=
+      match xs with
+      | nil => fys
+      | cons fz fzs => pure (cons fz (fzs >>= fun zs => append' zs fys))
+      end.
 
-Definition append A (fxs fys : Free S P (List S P A)) : Free S P (List S P A) :=
-  fxs >>= fun xs => append' xs fys.
+    Definition append A (fxs fys : Free S P (List S P A)) : Free S P (List S P A) :=
+      fxs >>= fun xs => append' xs fys.
+
+  End append.
+
+End FreeList.
+
 Module rose_map.
 
   Inductive List A :=
@@ -510,6 +548,9 @@ Module rose_map.
     Fail Fixpoint mapRose A B (f : A -> B) (r : Rose A) {struct r} : Rose B :=
       match r with
       | Leaf x      => Leaf (f x)
+      | Branches rs => Branches (map (fun x => mapRose f x) rs)
+      end.
+
   End wo_section.
 
   Module w_section.
@@ -551,7 +592,6 @@ Module rose_map.
       | Branches rs => Branches (map (mapRose f) rs)
       end.
 
-End append.
   End local_fix.
 
-End FreeList.End rose_map.
+End rose_map.
